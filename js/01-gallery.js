7633f212e561dd4b8b706bd393630052291a67ce
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-07-24 12:48:29 +0300

    first add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+
+const refs = {
+  ulEl: document.querySelector(".gallery"),
+};
+
+function onEscClose(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+
+  modal.close();
+}
+
+const imgEl = galleryItems
+  .map(({ preview, original, description}) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `)
+  .join("");
+
+
+refs.ulEl.insertAdjacentHTML("afterbegin", imgEl);
+
+
+const modal = basicLightbox.create(`
+  <img
+    src=""
+    width="800"
+    height="600"
+    alt="${galleryItems.description}"
+  />
+`, {
+  onShow: () => {
+        document.addEventListener("keydown", onEscClose);
+  },
+  onClose: () => {
+      document.removeEventListener("keydown", onEscClose);
+  },
+});
+
+function onOpenModal(evt) {
+  evt.preventDefault();
+ if (evt.target.nodeName !== "IMG") return; 
+  const source = evt.target.dataset.source;
+  const image = modal.element().querySelector("img");
+  image.src = source;
+  modal.show();
+}
+
+refs.ulEl.addEventListener("click", onOpenModal);
+
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  ulEl: document.querySelector(".gallery"),
+};
+
+const imgEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+     <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>
+`
+)
+    .join("");
+
+
+    console.log(galleryItems);
