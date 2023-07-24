197dc7ec80bdb11ff3f1c7730e04ba6c1df0c69c
DenShtephen <135002535+DenShtephen@users.noreply.github.com>
2023-07-24 13:20:01 +0300

    pre end

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,21 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 
-const refs = {
-  ulEl: document.querySelector(".gallery"),
-};
+
+const galleryList = document.querySelector('.gallery');
 
 const imgEl = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `
-  <li class="gallery__item">
+  .map(({ preview, original, description }) => { 
+  return `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
-     <img class="gallery__image" src="${preview}" alt="${description}" />
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
   </a>
-</li>
-`
-)
-    .join("");
+</li>`;
+}).join('');
 
+galleryList.insertAdjacentHTML("beforeend",imgEl);
 
-    console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  animationSpeed: 250,
+});
